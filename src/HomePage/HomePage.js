36f7474e4bb5bdde4f7d3aea2ecfd3dd86ee1f38
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-24 23:00:15 -0400

    shell for Home Page

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomePage() {
+
+  return (
+    <>
+      <main>
+        <div className="row justify-content-md-center">
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default HomePage;
