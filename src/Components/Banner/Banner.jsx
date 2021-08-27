7c3df27e55e611788ac297b4750cca66fd02c2b3
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-27 16:57:55 -0400

    updated banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,9 +1,26 @@
-import "./banner.scss"
+import "./banner.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 function Banner() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["Engineer", "Web Developer"],
+      backDelay: 1500,
+    });
+  }, [])
+
   return (
     <div className="banner" id="banner" role="img" aria-label="forest canopy from below">
-      <div className="item">My name is Stephanie Moore and I am a Full-Stack Engineer</div>
+      <div className="item">
+        <h2>Hello, I'm</h2>
+        <h1>Stephanie Moore</h1>
+        <h3>Full-Stack <span ref={textRef}></span></h3>
+      </div>
     </div>
   )
 }
