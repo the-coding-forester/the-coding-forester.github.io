449598f16a8bb26247044509b670037e16abc534
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-27 17:26:46 -0400

    Began work in intro body

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,3 +1,4 @@
+import Image from "react-bootstrap/Image";
 import MenuBar from "../MenuBar/MenuBar"
 import "./intro.scss"
 
@@ -5,6 +6,16 @@ function Intro() {
   return (
     <div className="intro" id="intro">
       <MenuBar />
+      <div className="item">
+        <div className="row">
+          <div className="column">Connect with Me</div>
+        </div>
+        <div className="row">
+          <div className="column"><Image src="src/Images/GitHub-Mark-64px.png" /></div>
+          <div className="column"><Image src="src/Images/LI-In-Bug.png" /></div>
+          <div className="column">Image</div>
+        </div>
+      </div>
     </div>
   )
 }
