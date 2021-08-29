598f79d6f437ad95056b43cd26be25a246b0374e
Stephanie Moore <smoore_forestry@outlook.com>
2021-08-28 23:24:25 -0400

    Updated connect with Me

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,15 @@
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 import Image from "react-bootstrap/Image";
+
 import MenuBar from "../MenuBar/MenuBar"
+
+import GitHubLogo from '../../Images/GitHub-Mark-64px.png';
+import LinkedInLogo from '../../Images/LI-In-Bug.png';
+import StackOverflowLogo from '../../Images/StackOverflow.png';
+
+
 import "./intro.scss"
 
 function Intro() {
@@ -7,14 +17,26 @@ function Intro() {
     <div className="intro" id="intro">
       <MenuBar />
       <div className="item">
-        <div className="row">
-          <div className="column">Connect with Me</div>
-        </div>
-        <div className="row">
-          <div className="column"><Image src="src/Images/GitHub-Mark-64px.png" /></div>
-          <div className="column"><Image src="src/Images/LI-In-Bug.png" /></div>
-          <div className="column">Image</div>
-        </div>
+        <Row>
+          <Col>Connect with Me</Col>
+        </Row>
+        <Row>
+          <Col>
+            <a href="https://github.com/the-coding-forester">
+              <Image className="image" src={GitHubLogo} alt="GitHub Icon" />
+            </a>
+          </Col>
+          <Col>
+            <a href="https://www.linkedin.com/in/the-coding-forester/">
+              <Image className="image" src={LinkedInLogo} alt="LinkedIn Icon" />
+            </a>
+          </Col>
+          <Col>
+            <a href="https://stackoverflow.com/users/15086797/the-coding-forester">
+              <Image className="image" src={StackOverflowLogo} alt="LinkedIn Icon" />
+            </a>
+          </Col>
+        </Row>
       </div>
     </div>
   )
